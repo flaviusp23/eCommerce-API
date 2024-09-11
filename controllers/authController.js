323be0682db42121a5a8e499f4f0229edbeab78b
flaviusp23 <flavius.paltin@yahoo.com>
2024-09-11 17:17:55 +0300

    email already exists

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,16 @@
+const { StatusCodes } = require('http-status-codes')
+const User = require('../models/User')
+const CustomError = require('../errors')
+
 const register = async(req,res) =>{
-    res.send('register')
+    const {email} = req.body
+
+    const emailAlreadyExists = await User.findOne({email})
+    if(emailAlreadyExists){
+        throw new CustomError.BadRequestError('Email already exists')
+    }
+    const user = await User.create(req.body);
+    res.status(StatusCodes.CREATED).json({user})
 }
 const login = async(req,res) =>{
     res.send('login')
