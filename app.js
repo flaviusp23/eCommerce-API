793edf9c7329e020693c73ea9399fd4f7d45bcb8
flaviusp23 <flavius.paltin@yahoo.com>
2024-09-11 17:00:22 +0300

    auth routes structure

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+require('dotenv').config()
+//express
 const express = require('express')
+const app = express()
+app.use(express.json())
 require('express-async-errors') // echivalent cu try catch, decat sa pun peste tot mai bine facem asa fiinda folosim async in controllers
+const port = process.env.PORT || 5000
+
 //db
 const connectDB = require('./db/connect')
 
@@ -8,16 +14,12 @@ const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddlware = require('./middleware/error-handler')
 const morgan = require('morgan')
 
-require('dotenv').config()
-
-const app = express()
-const port = process.env.PORT || 5000
-
-app.use(express.json())
-
 //logging requests
 app.use(morgan('tiny'))
 
+//routes
+const authRouter = require('./routes/authRoutes')
+app.use('/api/v1/auth',authRouter)
 app.get('/',(req,res)=>{
     res.send('Homepage')
 })
