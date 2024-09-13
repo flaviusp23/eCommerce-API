384f937c2d17bdcc86d9bb5ed98d9cb4057cac8c
flaviusp23 <flavius.paltin@yahoo.com>
2024-09-14 00:07:58 +0300

    authorization role improvement

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -5,23 +5,27 @@ const {isTokenValid} = require('../utils')
 const authenticateUser = async(req,res,next) =>{
     const token = req.signedCookies.token
     if(!token){
-        throw new CustomError.UnauthenticatedError('Autehentication Invalid')
+        throw new CustomError.UnauthenticatedError('Authentication Invalid')
     }
     try {
         const payload = isTokenValid({token});
         req.user = {name:payload.name,userId:payload.userId,role:payload.role}
         next()
     } catch (error) {
-        throw new CustomError.UnauthenticatedError('Autehentication Invalid')
+        throw new CustomError.UnauthenticatedError('Authentication Invalid')
     }
 }
 
-const authorizePermissions = async (req,res,next) =>{
-    if(req.user.role !== 'admin'){
-        throw new CustomError.UnauthorizedError('Unathorized to acces this route')
-    }
-    next();
-}
+const authorizePermissions = (...roles) => {
+    return (req, res, next) => {
+      if (!roles.includes(req.user.role)) {
+        throw new CustomError.UnauthorizedError(
+          'Unauthorized to access this route'
+        );
+      }
+      next();
+    };
+  };
 
 module.exports = {
     authenticateUser,
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,9 @@ const { getAllUsers, getSingleUser, showCurrentUser, updateUser, updateUserPassw
 const { authenticateUser, authorizePermissions } = require('../middleware/authentication')
 const router = express.Router()
 
-router.get('/',authenticateUser, authorizePermissions, getAllUsers)
-router.get('/showMe',showCurrentUser)
-router.patch('/updateUser',updateUser)
-router.patch('/updateUserPassword',updateUserPassword)
+router.get('/',authenticateUser, authorizePermissions('admin','owner'), getAllUsers)/*no owner but in future i can implement*/
+router.get('/showMe',authenticateUser,showCurrentUser)
+router.patch('/updateUser',authenticateUser,updateUser)
+router.patch('/updateUserPassword',authenticateUser,updateUserPassword)
 router.get('/:id',authenticateUser,getSingleUser)
 module.exports = router
