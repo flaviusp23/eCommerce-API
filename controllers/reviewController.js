8d10bb5e09ead44dff8b3ad0fa58cb8cd24ad9ad
flaviusp23 <flavius.paltin@yahoo.com>
2024-09-14 18:08:25 +0300

    .populate

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -25,7 +25,15 @@ const createReview = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ review });
 };
 const getAllReviews = async (req, res) => {
-  const reviews = await Review.find({});
+  const reviews = await Review.find({})
+    .populate({
+      path: "product",
+      select: "name company price",
+    })
+    .populate({
+      path: "user",
+      select: "name",
+    });
   res.status(StatusCodes.OK).json({ reviews, count: reviews.lenght });
 };
 const getSingleReview = async (req, res) => {
