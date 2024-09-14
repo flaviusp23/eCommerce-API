a7f876c21b9e95da7590c275869feee99cda6d2c
flaviusp23 <flavius.paltin@yahoo.com>
2024-09-14 23:11:55 +0300

    create order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,9 +5,60 @@ const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 const { checkPermissions } = require("../utils");
 
-const createOrder = async (req, res) => {
-  res.status(403).json({});
+const fakeStripeAPI = async ({ amount, currency }) => {
+  const client_secret = " someRandomValue";
+  return { client_secret, amount };
 };
+
+const createOrder = async (req, res) => {
+  const { items: cartItems, tax, shippingFee } = req.body;
+  if (!cartItems || cartItems.length < 1) {
+    throw new CustomError.BadRequestError("No cart items provided");
+  }
+  if (!tax || !shippingFee) {
+    throw new CustomError.BadRequestError(
+      "Please provide tax and shipping fee",
+    );
+  }
+  let orderItems = [];
+  let subtotal = 0;
+  for (const item of cartItems) {
+    const dbProduct = await Product.findOne({ _id: item.product });
+    if (!dbProduct) {
+      throw new CustomError.NotFoundError(
+        `No product with id: ${item.product}`,
+      );
+    }
+    const { name, price, image, _id } = dbProduct;
+    const singleOrderItem = {
+      amount: item.amount,
+      name,
+      price,
+      image,
+      product: _id,
+    };
+    orderItems.push(singleOrderItem);
+    subtotal += item.amount * price;
+  }
+  const total = tax + shippingFee + subtotal;
+  const paymentIntent = await fakeStripeAPI({
+    amount: total,
+    currency: "usd",
+  });
+  const order = await Order.create({
+    orderItems,
+    total,
+    subtotal,
+    tax,
+    shippingFee,
+    clientSecret: paymentIntent.client_secret,
+    user: req.user.userId,
+  });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ order, clientSecret: order.client_secret });
+};
+
 const getAllOrders = async (req, res) => {
   const orders = await Order.find({});
   res.status(403).json({ orders, count: orders.length });
